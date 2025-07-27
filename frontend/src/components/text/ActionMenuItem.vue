d89cd44772276042d001b618e26ed13990dc7018
Antonio Masotti <toniomasotti@gmail.com>
2025-07-27 15:29:40 +0200

    chore: typescript cleanup

--- frontend/src/components/text/ActionMenuItem.vue
+++ frontend/src/components/text/ActionMenuItem.vue
@@ -11,8 +11,8 @@
 </template>
 
 <script setup lang="ts">
-import { computed, h } from 'vue'
 import { combineClasses } from '@/styles/component-classes.ts'
+import { computed, h } from 'vue'
 import BaseIcon from '../common/BaseIcon.vue'
 
 interface Props {
