8dacbf1edca5fa2057c90cb9d87faccbc2554737
Antonio Masotti <toniomasotti@gmail.com>
2025-07-28 00:15:57 +0200

    feat: add sample data

---
+++ backend/src/main/resources/db/migration/V7__Data_Dump.sql
@@ -0,0 +1,124 @@
+--
+-- PostgreSQL database dump
+--
+
+--
+-- Data for Name: texts; Type: TABLE DATA; Schema: public; Owner: annahwi_user
+--
+INSERT INTO public.texts (id, title, arabic_content, transliteration, translation, tags, difficulty, dialect,
+                          word_count, comments, current_version, created_at, updated_at)
+VALUES ('a5500068-c9fa-417b-ba27-869e1f2b45c8', '!هاذي حومتنا', 'هاذي حومتنا! احنا نسكنوا في باردو، حومة كبيرة و معروفة في تونس العاصمة! حومتنا فيها برشا حاجات، فيها كل شي برسلك، فيها كيس كبير فيه رستورونات و قهاوي، وفيها سوق في اخر الحومة، و كل 200 ميترو فما ماغازان! و عندنا حجام كل 100 ميترو!
+
+انا كل يوم نشري كل شي من هنا! انا نمشي للخضار باش نشري خضرة! و نمشي للجزار باش نشري لحم، و نمشي للحوات باش نشري الحوت و نمشي للفارماسي كان نحب نشري دواء و للماغازان كان نحب نشري اي حاجة اخرى.
+
+كل نهار صباح نمشي نشري كرواسون و نمشي نشرب قهوتي في قهوة مزيانة قبل مانمشي نخدم، الخدمة مش بعيدة. في الحومة زادا و في القابلة نمشي للرستوران ناكل كفتاجي و لا لبابلي و نشرب كاس تاي.
+
+بعد الخدمة نمشي ديما للقهوة! في تونس الطقس ديما باهي الحمد لله كان في الصيف ديما نقعدو العشية لبرا خاطر فما ريح و مش سخون! و كان في الشتاء تقعدو لداخل خاطر سخونة! و في ربيع و الخريف كان مافماش مطر نقعد لبرا! و كان فما نقعدوا لداخل! مش مهم المفيد احنا ديما في القهوة مع الناس الي نحبوها!', 'Hādhī ḥūmtnā! Iḥnā nesknū fī Bārdū, ḥūma kbīra w maʿrūfa fī Tūnis il-ʿāsˤma! Ḥūmtnā fīhā barsha ḥājāt, fīhā kull shī berslek, fīhā kīs kbīr fīh restōrōnāt w qahāwī, w fīhā sūq fī ākher il-ḥūma, w kull mītrū fammā māghāzān! w ʿandnā ḥajjām kull mītrū!
+
+Anā kull yōm nishrī kull shī min hnā! Anā namshī lil-khaḍḍār bāsh nishrī khaḍra! w namshī lil-jazzār bāsh nishrī laḥm, w namshī lil-ḥawwāt bāsh nishrī il-ḥūt w namshī lil-farmāsī kān nḥibb nishrī dwā w lil-māghāzān kān nḥibb nishrī ēy ḥāja okhra.
+
+Kull nhār sˤbāḥ namshī nishrī krwāsōn w namshī nishrab qahwetī fī qahwa mazyēna qbal mā-namshī nakhdem, il-khidma mish bʿīda. Fī il-ḥūma zāda w fī il-qābila namshī lil-restōrān nākel keftājī w lā labāblī w nishrab kās tāy.
+
+Baʿd il-khidma namshī dīmā lil-qahwa! Fī Tūnis iṭ-ṭaqs dīmā bāhī l-ḥamdullāh kān fīṣ-ṣīf dīmā niqʿdū il-ʿashiyya libarrā khāṭer fammā rīḥ w mish skhūn! w kān fīsh-shitāʾ tiqʿdū liddākhel khāṭer skhūna! w fī rabīʿ w il-khrīf kān mā-fammāsh maṭar niqʿdū libarrā! w kān fammā niqʿdū liddākhel! Mish muhimm il-mufīd iḥnā dīmā fī il-qahwa maʿ in-nās illī nḥibbūhā!', 'Questo è il nostro quartiere! Noi viviamo a Bardo, un quartiere grande e famoso nella capitale Tunisi! Il nostro quartiere ha molte cose, ha tutto per te, ha una grande strada con ristoranti e caffè, e ha un mercato alla fine del quartiere, e ogni 200 metri c''è un negozio! E abbiamo un barbiere ogni 100 metri!
+
+Io ogni giorno compro tutto da qui! Vado dal fruttivendolo per comprare verdure! E vado dal macellaio per comprare carne, e vado dal pescivendolo per comprare pesce, e vado in farmacia se voglio comprare medicine, e al negozio se voglio comprare qualsiasi altra cosa.
+
+Ogni mattina vado a comprare un croissant e vado a bere il mio caffè in un bel caffè prima di andare a lavorare, il lavoro non è lontano. Nel quartiere anche, e di fronte, vado al ristorante a mangiare "kaftaji" o "lablabi" e bevo un bicchiere di tè.
+
+Dopo il lavoro andiamo sempre al caffè! In Tunisia il tempo è sempre bello, grazie a Dio, tranne in estate stiamo sempre seduti fuori la sera perché c''è vento e non è caldo! E se è inverno stiamo seduti dentro perché fa caldo (per il riscaldamento)! E in primavera e autunno se non c''è pioggia stiamo seduti fuori! E se c''è (pioggia) stiamo seduti dentro! Non è importante, l''importante è che siamo sempre al caffè con le persone che amiamo!',
+        '[
+          "lifestyle",
+          "weather",
+          "shopping"
+        ]', 'BEGINNER', 'TUNISIAN', 181, NULL, NULL, '2025-07-27 23:28:48.931536', '2025-07-27 23:28:48.931545');
+
+INSERT INTO public.texts (id, title, arabic_content, transliteration, translation, tags, difficulty, dialect,
+                          word_count, comments, current_version, created_at, updated_at)
+VALUES ('f02d673d-26d7-420a-9809-3d917f59d150', 'What I wear and when', 'أنا في طبعي انسان بسيط برشا في لبستي. ما نحبش نصرف برشا فلوس على الحوايج، خاطر نشوفها موش أهم حاجة في الدنيا. أهم حاجة عندي هي الراحة، موش الستيل ولا الموضة. بالحق ما نركزش برشا في الألوان اما بصفة عامة نختار ألوان محايدة كيما الأكحل، كيما gris ولا الأبيض. ساهلين باش تلبسهم مع بعضهم.
+في الشتاء الطقس في ألمانيا بارد يقتل، على هذاكا ديما نلبس فيستة غليظة باش نبقى دافي. تحت الفيستة نلبس مريول صوف سخون وسروال طويل، عادة يكون جينز. ما نحبش نلبس برشا طبقات.
+
+في الصيف الحكاية تتبدل بالكل. نلبس كان بولو خفيف وشورطات. نحب الألوان الفاتحة كيف الأبيض ولا البيج، خاطرها ما تشدش السخانة. أما ساعات يعجبني لون معين، فنشري بولو بلون أخر، مثلاََ أزرق، بلو سيل ولا أخضر. المهم يكون بولو قطن و مرتاح. وفي ساقيّا نلبس صندل خفيف.
+
+اما في الربيع ولا في الخريف، الجو مرة باهي، مرة خايب، يعني متقلب. في الوقت هذا، الحل المثالي بالنسبة ليا هو سوريّة. نحب برشا نلبس سوريّة بسيط، مثلاََ أبيض ولا أزرق، مع جينز وسبادري. نشوفها لبسة أنيقة وبراتيك في نفس الوقت، تنجم تمشي بيها للخدمة ولا تخرج بيها مع أصحابك. ما نحبش نعقدها برشا. في الآخر، اللبسة لازمها تكون حاجة تخليك تحس روحك مرتاح في حياتك اليومية، موش مصدر قلق وفلوس ضايعة.', 'Anā fī ṭabʿī insēn bsīṭ barsha fī libistī. Mā nḥebbsh naṣraf barsha flūs ʿalā el-ḥwāyej, khāṭer nshūfhā mūsh ahamm ḥāja fī d-dunyā. Ahamm ḥāja ʿandī hiya er-rāḥa, mūsh es-stīl wela el-mōḍa. Bel-ḥaqq mā narkkzesh barsha fīl-alwān amma b-ṣifa ʿāmma nakhtār alwān muḥāyda kīma el-akḥal, kīma esh-shakhma wela el-abyaḍ. Sāhlīn bāsh tilbishom mʿa bʿaḍhom.
+Fīsh-shtēʾ eṭ-ṭaqs fī Almānya bāred yoqtel, ʿalā hādhāka dīma nilbes jāketāt ghlīḍha bāsh nibqā dēfī. Taḥt el-jāketāt nilbes maryūl ṣūf skhūn w-srwēl ṭwīl, ʿādatan ykūn jīnz. Mā nḥebbsh nilbes barsha ṭabaqāt bāsh mā nitfallaqsh.
+Fīṣ-ṣīf el-ḥkāya titbaddel bel-koll. Nilbes kēn pōlō khafīf w-shorṭāt. Nḥebb el-alwān el-fētḥa kīf el-abyaḍ wela el-bēj, khāṭerhā mā tsheddsh es-skhāna. Amma sāʿāt yaʿjebnī lōn mʿayyen, fa-nishrī pōlō b-lōn ākher, mathalan azraq, blū syēl wela akhḍar. El-muhem ykūn pōlō qṭon w-mrīyaḥ. W-fī sāqayya nilbes ṣandāl khafīf.
+Amma fīr-rbīʿ wela fīl-khrīf, el-jaww marra bēhī, marra khāyeb, yaʿnī mutaqalleb. Fīl-waqt hādha, el-ḥall el-mithālī ben-nisba līya huwa el-qomṣān. Nḥebb barsha nilbes qamīṣ bsīṭ, mathalan abyaḍ wela azraq, mʿa jīnz w-sbadrī. Nshūfhā libsa anīqa w-ʿamaliyya fī nafs el-waqt, tnajjem timshī bīhā lel-khedma wela tokhroj bīhā mʿa aṣḥābek. Mā nḥebbsh nʿaqqedhā barsha. Fil-lekhkhar, el-libsa lāzemhā tkūn ḥāja tkhallīk tḥess rūḥek mirtēḥ fī ḥayātek el-yawmiyya, mūsh maṣdar qalaq w-flūs ḍāyʿa.', 'Di natura sono una persona molto semplice nel mio modo di vestire. Non mi piace spendere molti soldi per i vestiti, perché non la vedo come la cosa più importante al mondo. La cosa più importante per me è la comodità, non lo stile o la moda. Davvero non mi concentro molto sui colori, ma in generale scelgo colori neutri come il nero, il grigio antracite o il bianco. Sono facili da abbinare tra loro.
+In inverno, il tempo in Germania è freddo da morire, per questo indosso sempre giacche pesanti per rimanere al caldo. Sotto le giacche indosso un maglione di lana caldo e pantaloni lunghi, di solito jeans. Non mi piace indossare troppi strati per non "scoppiare" (sentirmi costretto e a disagio).
+In estate, la storia cambia completamente. Indosso solo polo leggere e pantaloncini. Mi piacciono i colori chiari come il bianco o il beige, perché non trattengono il calore. Ma a volte mi piace un colore specifico, quindi compro una polo di un altro colore, per esempio blu, azzurro cielo o verde. L''importante è che la polo sia di cotone e comoda. E ai piedi indosso sandali leggeri.
+Ma in primavera o in autunno, il tempo è a volte bello, a volte brutto, cioè variabile. In questo periodo, la soluzione ideale per me sono le camicie. Mi piace molto indossare una camicia semplice, per esempio bianca o blu, con jeans ed espadrillas/sneakers. La considero un abbigliamento elegante e pratico allo stesso tempo, con cui puoi andare al lavoro o uscire con i tuoi amici. Non mi piace complicare troppo le cose. Alla fine, l''abbigliamento deve essere qualcosa che ti fa sentire a tuo agio nella vita di tutti i giorni, non una fonte di preoccupazione e di soldi sprecati.',
+        '[
+          "weather",
+          "clothes",
+          "colors",
+          "lifestyle"
+        ]', 'INTERMEDIATE', 'TUNISIAN', 205, NULL, NULL, '2025-07-27 23:30:46.554914', '2025-07-27 23:30:46.554916');
+
+---
+-- Versions
+--
+INSERT INTO public.text_versions (id, text_id, version_number, content, created_at, updated_at, is_current)
+VALUES ('3793ac1b-1436-4100-a70f-4e38ecdcfb69', 'a5500068-c9fa-417b-ba27-869e1f2b45c8', 1, '{
+  "tags": [
+    "lifestyle",
+    "weather",
+    "shopping"
+  ],
+  "title": "!هاذي حومتنا",
+  "dialect": "TUNISIAN",
+  "wordCount": 181,
+  "difficulty": "BEGINNER",
+  "translation": "Questo è il nostro quartiere! Noi viviamo a Bardo, un quartiere grande e famoso nella capitale Tunisi! Il nostro quartiere ha molte cose, ha tutto per te, ha una grande strada con ristoranti e caffè, e ha un mercato alla fine del quartiere, e ogni 200 metri c''è un negozio! E abbiamo un barbiere ogni 100 metri!\n\nIo ogni giorno compro tutto da qui! Vado dal fruttivendolo per comprare verdure! E vado dal macellaio per comprare carne, e vado dal pescivendolo per comprare pesce, e vado in farmacia se voglio comprare medicine, e al negozio se voglio comprare qualsiasi altra cosa.\n\nOgni mattina vado a comprare un croissant e vado a bere il mio caffè in un bel caffè prima di andare a lavorare, il lavoro non è lontano. Nel quartiere anche, e di fronte, vado al ristorante a mangiare \"kaftaji\" o \"lablabi\" e bevo un bicchiere di tè.\n\nDopo il lavoro andiamo sempre al caffè! In Tunisia il tempo è sempre bello, grazie a Dio, tranne in estate stiamo sempre seduti fuori la sera perché c''è vento e non è caldo! E se è inverno stiamo seduti dentro perché fa caldo (per il riscaldamento)! E in primavera e autunno se non c''è pioggia stiamo seduti fuori! E se c''è (pioggia) stiamo seduti dentro! Non è importante, l''importante è che siamo sempre al caffè con le persone che amiamo!",
+  "arabicContent": "هاذي حومتنا! احنا نسكنوا في باردو، حومة كبيرة و معروفة في تونس العاصمة! حومتنا فيها برشا حاجات، فيها كل شي برسلك، فيها كيس كبير فيه رستورونات و قهاوي، وفيها سوق في اخر الحومة، و كل 200 ميترو فما ماغازان! و عندنا حجام كل 100 ميترو!\n\nانا كل يوم نشري كل شي من هنا! انا نمشي للخضار باش نشري خضرة! و نمشي للجزار باش نشري لحم، و نمشي للحوات باش نشري الحوت و نمشي للفارماسي كان نحب نشري دواء و للماغازان كان نحب نشري اي حاجة اخرى.\n\nكل نهار صباح نمشي نشري كرواسون و نمشي نشرب قهوتي في قهوة مزيانة قبل مانمشي نخدم، الخدمة مش بعيدة. في الحومة زادا و في القابلة نمشي للرستوران ناكل كفتاجي و لا لبابلي و نشرب كاس تاي.\n\nبعد الخدمة نمشي ديما للقهوة! في تونس الطقس ديما باهي الحمد لله كان في الصيف ديما نقعدو العشية لبرا خاطر فما ريح و مش سخون! و كان في الشتاء تقعدو لداخل خاطر سخونة! و في ربيع و الخريف كان مافماش مطر نقعد لبرا! و كان فما نقعدوا لداخل! مش مهم المفيد احنا ديما في القهوة مع الناس الي نحبوها!",
+  "transliteration": "Hādhī ḥūmtnā! Iḥnā nesknū fī Bārdū, ḥūma kbīra w maʿrūfa fī Tūnis il-ʿāsˤma! Ḥūmtnā fīhā barsha ḥājāt, fīhā kull shī berslek, fīhā kīs kbīr fīh restōrōnāt w qahāwī, w fīhā sūq fī ākher il-ḥūma, w kull mītrū fammā māghāzān! w ʿandnā ḥajjām kull mītrū!\n\nAnā kull yōm nishrī kull shī min hnā! Anā namshī lil-khaḍḍār bāsh nishrī khaḍra! w namshī lil-jazzār bāsh nishrī laḥm, w namshī lil-ḥawwāt bāsh nishrī il-ḥūt w namshī lil-farmāsī kān nḥibb nishrī dwā w lil-māghāzān kān nḥibb nishrī ēy ḥāja okhra.\n\nKull nhār sˤbāḥ namshī nishrī krwāsōn w namshī nishrab qahwetī fī qahwa mazyēna qbal mā-namshī nakhdem, il-khidma mish bʿīda. Fī il-ḥūma zāda w fī il-qābila namshī lil-restōrān nākel keftājī w lā labāblī w nishrab kās tāy.\n\nBaʿd il-khidma namshī dīmā lil-qahwa! Fī Tūnis iṭ-ṭaqs dīmā bāhī l-ḥamdullāh kān fīṣ-ṣīf dīmā niqʿdū il-ʿashiyya libarrā khāṭer fammā rīḥ w mish skhūn! w kān fīsh-shitāʾ tiqʿdū liddākhel khāṭer skhūna! w fī rabīʿ w il-khrīf kān mā-fammāsh maṭar niqʿdū libarrā! w kān fammā niqʿdū liddākhel! Mish muhimm il-mufīd iḥnā dīmā fī il-qahwa maʿ in-nās illī nḥibbūhā!"
+}', '2025-07-27 23:28:48.932363', '2025-07-27 23:28:48.932365', true);
+INSERT INTO public.text_versions (id, text_id, version_number, content, created_at, updated_at, is_current)
+VALUES ('95bba6bd-7d4f-4d3c-b84c-43c757ff1ad3', 'f02d673d-26d7-420a-9809-3d917f59d150', 1, '{
+  "tags": [
+    "weather",
+    "clothes",
+    "colors",
+    "lifestyle"
+  ],
+  "title": "What I wear and when",
+  "dialect": "TUNISIAN",
+  "wordCount": 205,
+  "difficulty": "INTERMEDIATE",
+  "translation": "Di natura sono una persona molto semplice nel mio modo di vestire. Non mi piace spendere molti soldi per i vestiti, perché non la vedo come la cosa più importante al mondo. La cosa più importante per me è la comodità, non lo stile o la moda. Davvero non mi concentro molto sui colori, ma in generale scelgo colori neutri come il nero, il grigio antracite o il bianco. Sono facili da abbinare tra loro.\nIn inverno, il tempo in Germania è freddo da morire, per questo indosso sempre giacche pesanti per rimanere al caldo. Sotto le giacche indosso un maglione di lana caldo e pantaloni lunghi, di solito jeans. Non mi piace indossare troppi strati per non \"scoppiare\" (sentirmi costretto e a disagio).\nIn estate, la storia cambia completamente. Indosso solo polo leggere e pantaloncini. Mi piacciono i colori chiari come il bianco o il beige, perché non trattengono il calore. Ma a volte mi piace un colore specifico, quindi compro una polo di un altro colore, per esempio blu, azzurro cielo o verde. L''importante è che la polo sia di cotone e comoda. E ai piedi indosso sandali leggeri.\nMa in primavera o in autunno, il tempo è a volte bello, a volte brutto, cioè variabile. In questo periodo, la soluzione ideale per me sono le camicie. Mi piace molto indossare una camicia semplice, per esempio bianca o blu, con jeans ed espadrillas/sneakers. La considero un abbigliamento elegante e pratico allo stesso tempo, con cui puoi andare al lavoro o uscire con i tuoi amici. Non mi piace complicare troppo le cose. Alla fine, l''abbigliamento deve essere qualcosa che ti fa sentire a tuo agio nella vita di tutti i giorni, non una fonte di preoccupazione e di soldi sprecati.",
+  "arabicContent": "أنا في طبعي انسان بسيط برشا في لبستي. ما نحبش نصرف برشا فلوس على الحوايج، خاطر نشوفها موش أهم حاجة في الدنيا. أهم حاجة عندي هي الراحة، موش الستيل ولا الموضة. بالحق ما نركزش برشا في الألوان اما بصفة عامة نختار ألوان محايدة كيما الأكحل، كيما gris ولا الأبيض. ساهلين باش تلبسهم مع بعضهم.\nفي الشتاء الطقس في ألمانيا بارد يقتل، على هذاكا ديما نلبس فيستة غليظة باش نبقى دافي. تحت الفيستة نلبس مريول صوف سخون وسروال طويل، عادة يكون جينز. ما نحبش نلبس برشا طبقات.\n\nفي الصيف الحكاية تتبدل بالكل. نلبس كان بولو خفيف وشورطات. نحب الألوان الفاتحة كيف الأبيض ولا البيج، خاطرها ما تشدش السخانة. أما ساعات يعجبني لون معين، فنشري بولو بلون أخر، مثلاََ أزرق، بلو سيل ولا أخضر. المهم يكون بولو قطن و مرتاح. وفي ساقيّا نلبس صندل خفيف.\n\nاما في الربيع ولا في الخريف، الجو مرة باهي، مرة خايب، يعني متقلب. في الوقت هذا، الحل المثالي بالنسبة ليا هو سوريّة. نحب برشا نلبس سوريّة بسيط، مثلاََ أبيض ولا أزرق، مع جينز وسبادري. نشوفها لبسة أنيقة وبراتيك في نفس الوقت، تنجم تمشي بيها للخدمة ولا تخرج بيها مع أصحابك. ما نحبش نعقدها برشا. في الآخر، اللبسة لازمها تكون حاجة تخليك تحس روحك مرتاح في حياتك اليومية، موش مصدر قلق وفلوس ضايعة.",
+  "transliteration": "Anā fī ṭabʿī insēn bsīṭ barsha fī libistī. Mā nḥebbsh naṣraf barsha flūs ʿalā el-ḥwāyej, khāṭer nshūfhā mūsh ahamm ḥāja fī d-dunyā. Ahamm ḥāja ʿandī hiya er-rāḥa, mūsh es-stīl wela el-mōḍa. Bel-ḥaqq mā narkkzesh barsha fīl-alwān amma b-ṣifa ʿāmma nakhtār alwān muḥāyda kīma el-akḥal, kīma esh-shakhma wela el-abyaḍ. Sāhlīn bāsh tilbishom mʿa bʿaḍhom.\nFīsh-shtēʾ eṭ-ṭaqs fī Almānya bāred yoqtel, ʿalā hādhāka dīma nilbes jāketāt ghlīḍha bāsh nibqā dēfī. Taḥt el-jāketāt nilbes maryūl ṣūf skhūn w-srwēl ṭwīl, ʿādatan ykūn jīnz. Mā nḥebbsh nilbes barsha ṭabaqāt bāsh mā nitfallaqsh.\nFīṣ-ṣīf el-ḥkāya titbaddel bel-koll. Nilbes kēn pōlō khafīf w-shorṭāt. Nḥebb el-alwān el-fētḥa kīf el-abyaḍ wela el-bēj, khāṭerhā mā tsheddsh es-skhāna. Amma sāʿāt yaʿjebnī lōn mʿayyen, fa-nishrī pōlō b-lōn ākher, mathalan azraq, blū syēl wela akhḍar. El-muhem ykūn pōlō qṭon w-mrīyaḥ. W-fī sāqayya nilbes ṣandāl khafīf.\nAmma fīr-rbīʿ wela fīl-khrīf, el-jaww marra bēhī, marra khāyeb, yaʿnī mutaqalleb. Fīl-waqt hādha, el-ḥall el-mithālī ben-nisba līya huwa el-qomṣān. Nḥebb barsha nilbes qamīṣ bsīṭ, mathalan abyaḍ wela azraq, mʿa jīnz w-sbadrī. Nshūfhā libsa anīqa w-ʿamaliyya fī nafs el-waqt, tnajjem timshī bīhā lel-khedma wela tokhroj bīhā mʿa aṣḥābek. Mā nḥebbsh nʿaqqedhā barsha. Fil-lekhkhar, el-libsa lāzemhā tkūn ḥāja tkhallīk tḥess rūḥek mirtēḥ fī ḥayātek el-yawmiyya, mūsh maṣdar qalaq w-flūs ḍāyʿa."
+}', '2025-07-27 23:30:46.555196', '2025-07-27 23:30:46.555198', true);
+
+
+
+
+
+
+
+--
+-- Data for Name: annotations; Type: TABLE DATA; Schema: public; Owner: annahwi_user
+--
+INSERT INTO public.annotations (id, text_id, content, type, color, created_at, anchor_text, mastery_level, needs_review,
+                                next_review_date)
+VALUES ('2824ddfa-7da8-4d2b-8096-2f473f148c4d', 'f02d673d-26d7-420a-9809-3d917f59d150', '"fi tab3i" = by nature, per carattere. Compare طْبِيعَة (tabi3a), "nature" or بِالطْبِيعَة (bi-tabi3a) = of course
+', 'VOCABULARY', '#32a7cf', '2025-07-27 23:33:18.480112', 'في طبعي', 'LEARNING', true, NULL);
+INSERT INTO public.annotations (id, text_id, content, type, color, created_at, anchor_text, mastery_level, needs_review,
+                                next_review_date)
+VALUES ('ea5c2c6d-0a61-4f0c-8baf-2cda8b6b5457', 'f02d673d-26d7-420a-9809-3d917f59d150', 'nosfor = spend (waste) money',
+        'VOCABULARY', '#32a7cf', '2025-07-27 23:34:54.953488', 'نصرف', 'NEW', true, NULL);
+INSERT INTO public.annotations (id, text_id, content, type, color, created_at, anchor_text, mastery_level, needs_review,
+                                next_review_date)
+VALUES ('603650fa-e90a-45ea-87b6-c5a317016417', 'f02d673d-26d7-420a-9809-3d917f59d150',
+        'nshuf-ha, notice the use of the pronoun. Very typical tunisian.', 'GRAMMAR', '#47cf32',
+        '2025-07-27 23:35:26.875531', 'نشوفها', 'KNOWN', false, NULL);
+INSERT INTO public.annotations (id, text_id, content, type, color, created_at, anchor_text, mastery_level, needs_review,
+                                next_review_date)
+VALUES ('d91f59de-3189-4ebc-85cc-e67ff443558a', 'f02d673d-26d7-420a-9809-3d917f59d150',
+        'ahamm = superlative of muhim (مهم), important', 'GRAMMAR', '#47cf32', '2025-07-27 23:36:19.841217', 'أهم',
+        'KNOWN', false, NULL);
+INSERT INTO public.annotations (id, text_id, content, type, color, created_at, anchor_text, mastery_level, needs_review,
+                                next_review_date)
+VALUES ('9551a18b-2735-47ea-8c87-0ba6ff8a4513', 'f02d673d-26d7-420a-9809-3d917f59d150', 'bel-7aq = in verità / onestamente, a die il vero.
+
+Anche usato: باكل صراحة = bikull sara7a = in tutta onestà.', 'VOCABULARY', '#32a7cf', '2025-07-27 23:37:55.219269',
+        'بالحق', 'LEARNING', true, NULL);
+
