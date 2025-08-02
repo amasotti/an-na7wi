055b94525ac8f51af32db3e1c49803c5b6cb353f
Antonio Masotti <toniomasotti@gmail.com>
2025-08-02 23:20:07 +0200

    feat: move to frontend dir

--- frontend-nuxt/composables/api.ts
+++
@@ -1,8 +0,0 @@
-// Use the API client from the Nuxt plugin
-export const useApiClient = () => {
-  const { $apiClient } = useNuxtApp()
-  return $apiClient
-}
-
-// Default export for backward compatibility
-export default useApiClient
--- frontend-nuxt/types/index.ts
+++
@@ -1,6 +0,0 @@
-// Re-export all types from their respective modules
-export * from './enums'
-export * from './entities'
-export * from './api'
-export * from './ui'
-export * from './textVersion'
