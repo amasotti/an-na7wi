34ea6aab06c4a278a9703eeb4d91a346f243aa38
Antonio Masotti <toniomasotti@gmail.com>
2025-11-16 15:00:07 +0100

    chore: improve edit btn (add more props)

--- frontend/components/common/EditButton.vue
+++ frontend/components/common/EditButton.vue
@@ -2,19 +2,36 @@
   <BaseButton
     variant="outline"
     size="sm"
+    :disabled="disabled"
+    :loading="loading"
     class="edit-button"
     @click="$emit('click')"
   >
     <BaseIcon size="sm" class="mr-2">
       <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
     </BaseIcon>
-    <slot>Edit</slot>
+    <slot>{{ text }}</slot>
   </BaseButton>
 </template>
 
 <script setup lang="ts">
 import BaseButton from '~/components/common/BaseButton.vue'
 import BaseIcon from '~/components/common/BaseIcon.vue'
+import type {ButtonRole} from "~/types";
+
+interface EditBtnProps {
+  text?: string
+  loading?: boolean
+  disabled?: boolean
+  type?: ButtonRole
+}
+
+withDefaults(defineProps<EditBtnProps>(), {
+  text: 'Edit',
+  loading: false,
+  disabled: false,
+  type: 'button',
+})
 
 defineEmits<{
   click: []
